c1428c758823bb1fa9d7324cb468cda981602a9c
Spacecowboi <cunningham2192@gmail.com>
2023-10-03 22:17:21 -0700

    finished implementing tests

--- clients/vendor/vendor-handler.test.js
+++ clients/vendor/vendor-handler.test.js
@@ -1,20 +1,25 @@
 'use strict';
 //tests created using ChatGPT 3.5, very interesting methods of testing like spyOn
+//tests for socket made using ChatGPT 3.5
 
-const eventPool = require('../eventpool.js');
+const io = require('socket.io-client');
 const vendor = require('./handler.js');
 
-jest.spyOn(eventPool, 'emit');
-jest.spyOn(console, 'log');
+jest.mock('socket.io-client');
 
 describe('Vendor handler', () => {
-    it('should emit a pickup event when an order is made', () => {
-        vendor.order('Somestore', '123', 'Jane Doe');
-        expect(eventPool.emit).toHaveBeenCalledWith('pickup', { storeName: 'Somestore', orderId: '123', customerName: 'Jane Doe'});
+    let mockEmit;
+
+    beforeEach(() => {
+        mockEmit = jest.fn();
+        io.connect.mockReturnValue({
+            emit: mockEmit,
+            on: jest.fn(),
+        });
     });
 
-    it('should log a thank you message when a delivered event is received', () => {
-        eventPool.emit('delivered', { storeName: 'Somestore', orderId: '123', customerName: 'Jane Doe'});
-        expect(console.log).toHaveBeenCalledWith('Vendor: Thank you for your order, Jane Doe');
+    it('should emit a pickup event when an order is made', () => {
+        vendor.order('Somestore', '123', 'Jane Doe');
+        expect(mockEmit).toHaveBeenCalledWith('pickup', { storeName: 'Somestore', orderId: '123', customerName: 'Jane Doe'});
     });
 });
